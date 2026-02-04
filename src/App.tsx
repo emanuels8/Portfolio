2ccaef8b47c27ed6292664fe600bf01d9291cb6c
dns-emanuels <emmanuels8001@gmail.com>
2026-02-04 03:59:23 -0600

    feat: Add router and route to projects

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Hero from "./components/Hero";
+import Hero from "./components/hero/Hero";
+import Projects from "./components/Projects";
 
 const App: React.FC = () => {
   return (
-    <div className="flex min-h-screen items-center justify-center">
-      <Hero />
-    </div>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <div className="flex min-h-screen items-center justify-center">
+            <Hero />
+          </div>
+        }
+      />
+      <Route
+        path="/projects"
+        element={
+          <div className="flex min-h-screen items-center justify-center">
+            <Projects />
+          </div>
+        }
+      />
+    </Routes>
   );
 };
 
